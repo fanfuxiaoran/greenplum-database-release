dc9da5a7bcaf8635ec72fecbe6b01b67e19f2277
Ning Wu <ningw@vmware.com>
2020-07-03 15:11:35 +0800

    Support creating DEB package for gpdb5
    Authored-by: Ning Wu <ningw@vmware.com>

---
+++ ci/concourse/scripts/ppa-debian-release.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DEBIAN_RELEASE_DIR="greenplum-database-release"
+GPDB_SRC_DIR="gpdb"
+GPDB_VERSION_SHORT=$(${GPDB_SRC_DIR}/getversion --short)
+
+gpg --import <(echo "$GPG_PRIVATE_KEY")
+
+set -x
+
+# depends on debmake and other tools being available already in image
+
+# Regex to capture required gporca version and download gporca source
+ORCA_TAG=$(grep -Po 'v\d+.\d+.\d+' ${GPDB_SRC_DIR}/depends/conanfile_orca.txt)
+git clone --branch "${ORCA_TAG}" https://github.com/greenplum-db/gporca.git ${GPDB_SRC_DIR}/gporca
+
+mv ${DEBIAN_RELEASE_DIR}/debian ${GPDB_SRC_DIR}/
+
+# Create a changelog
+pushd ${GPDB_SRC_DIR}
+git rev-parse --short HEAD >BUILD_NUMBER
+dch --create --package greenplum-db-5 -v "${GPDB_VERSION_SHORT}" "${RELEASE_MESSAGE}"
+dch -r "ignored message"
+popd
+GPDB_VERSION_LONG=$(${GPDB_SRC_DIR}/getversion)
+
+tar czf greenplum-db-5_"${GPDB_VERSION_SHORT}".orig.tar.gz ${GPDB_SRC_DIR}
+
+# Generate source.changes file
+pushd ${GPDB_SRC_DIR}
+debuild -S -sa
+popd
+
+# Upload source.changes and source.tar.gz to PPA repository
+dput "${PPA_REPO}" greenplum-db-5_"${GPDB_VERSION_SHORT}"_source.changes >/dev/null
+
+echo "Finished Uploading"
+echo "Greenplum short version: $GPDB_VERSION_SHORT"
+echo "Greenplum long version:  $GPDB_VERSION_LONG"
